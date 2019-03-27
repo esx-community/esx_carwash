fc4b7b184c5db340bd78f4fba3f9a4c0b812e9be
BackrndSource <back.rndsource@gmail.com>
2019-03-27 11:40:15 +0000

    Create es.lua

---
+++ locales/es.lua
@@ -0,0 +1,9 @@
+Locales['es'] = {
+  ['prompt_wash'] = 'pulsa ~INPUT_CONTEXT~ para lavar el vehículo',
+  ['prompt_wash_paid'] = 'pulsa ~INPUT_CONTEXT~ para lavar el vehículo por ~g~%s€~s~',
+  ['wash_failed'] = 'no tienes dinero suficiente...',
+  ['wash_failed_clean'] = 'tu vehículo no necesita un lavado...',
+  ['wash_successful'] = 'tu vehículo ha sido lavado !',
+  ['wash_successful_paid'] = 'tu vehículo ha sido lavado por ~g~%s€~s~',
+  ['blip_carwash'] = 'Lavado de coches',
+}
