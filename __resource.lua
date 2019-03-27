784b7ad3d939db000d25b97a2e71a683a03f4eb7
BackrndSource <back.rndsource@gmail.com>
2019-03-27 11:41:02 +0000

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -7,6 +7,7 @@ version '1.0.0'
 server_scripts {
 	'@es_extended/locale.lua',
 	'locales/en.lua',
+	'locales/es.lua',
 	'locales/sv.lua',
 	'config.lua',
 	'server/main.lua'
@@ -15,6 +16,7 @@ server_scripts {
 client_scripts {
 	'@es_extended/locale.lua',
 	'locales/en.lua',
+	'locales/es.lua',
 	'locales/sv.lua',
 	'config.lua',
 	'client/main.lua'
