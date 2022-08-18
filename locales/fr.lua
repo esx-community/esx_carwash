74249e36263f40727365f5f829242935c26185c6
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2022-08-18 17:36:29 +0200

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,9 @@
+Locales['fr'] = {
+  ['prompt_wash'] = 'Pressé ~INPUT_CONTEXT~ laver ce véhicule',
+  ['prompt_wash_paid'] = 'Pressé ~INPUT_CONTEXT~ pour laver ce véhicule cette action vous coutera ~g~$%s~s~',
+  ['wash_failed'] = 'Vous ne pouvez pas vous permettre un lavage de voiture',
+  ['wash_failed_clean'] = 'Votre véhicule n\'a pas besoin d\'un lavage',
+  ['wash_successful'] = 'Votre véhicule a été lavé',
+  ['wash_successful_paid'] = 'Votre véhicule a été lavé pour un total de  ~g~$%s~s~',
+  ['blip_carwash'] = 'Lave-Auto',
+}
