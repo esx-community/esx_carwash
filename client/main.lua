ad5d10c91068ccc005e1969d8ade6ccd7bf1a4e4
Flamba <82859879+Flambaa@users.noreply.github.com>
2023-01-19 00:21:27 +1100

    Refactor(esx-carwash): use export getSharedObject
    Remove depreciated esx getSharedObject trigger event, change to export

--- client/main.lua
+++ client/main.lua
@@ -1,11 +1,4 @@
-ESX = nil
-
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
+ESX = exports["es_extended"]:getSharedObject()
 
 Citizen.CreateThread(function()
 	for i=1, #Config.Locations, 1 do
